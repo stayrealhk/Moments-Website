369798e469d5559e34049ad5ece2c241ee0cf5d2
Harry Ng <harryworld@gmail.com>
2015-12-25 23:27:11 +0800

    Update layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
 var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("#polina button");
+var pauseButton = document.querySelector("#moments button");
 
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed
+vid.pause();
+// to capture IE10
+vidFade();
+});
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    navigation: true,
+    afterLoad: function(anchorLink, index) {
+      if (index == 1) {
+        $('#fp-nav span').css('backgroundColor', 'white');
+      }
+    },
+    onLeave: function(index, nextIndex) {
+      if (nextIndex == 1) {
+        $('footer').animate({
+          backgroundColor: 'transparent',
+          borderTop: 'transparent'
+        });
+        $('#fp-nav span').animate({
+          backgroundColor: 'white'
+        });
+      }
+      if (index == 1) {
+        $('footer').animate({
+          backgroundColor: 'rgba(155,155,155,0.5)',
+          borderTop: 'rgba(155,155,155,0.9)'
+        });
+        $('#fp-nav span').animate({
+          backgroundColor: '#F98100'
+        });
+      }
+    }
+  });
+});
